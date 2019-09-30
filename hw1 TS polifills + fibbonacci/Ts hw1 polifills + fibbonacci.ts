45af6f587b7d6f76b9c292c75724c062832f6eff
Dmitriy Barbul <doomhory@gmail.com>
2019-09-30 19:50:46 +0300

    changes for hw1 TS

--- hw1 TS polifills + fibbonacci/Ts hw1 polifills + fibbonacci.ts
+++ hw1 TS polifills + fibbonacci/Ts hw1 polifills + fibbonacci.ts
@@ -1,11 +1,10 @@
 //Task №1 polindrom 
 
-function polindrom(str: string) {
-  if (str == str.split('').reverse().join('')) {
+function polindrom(str: string): string {
+  if (str == str.split('').reverse().join('')) { 
     return ` yes it is a pollindrom! - ${str}`
-  } else {
-    return  `it's not a pollindrom!`
   }
+  return `no it is not a polindrom! - ${str}`
  };
  
  console.log(polindrom('lol'));
@@ -13,22 +12,17 @@ function polindrom(str: string) {
 
  //Task №2 Fibbonacci
 
- function fib(num: number) {
-  if (num > 0) {
-    let arr: Array<number> = []
-    let a: number = 1;
-    let b: number = 1;
-      for (let i: number = 0; i <= num; i++) {
-        let c: number = a + b;
-        a = b;
-        b = c;
-        arr.push(b)
-    }
+ function fib(num: number): Array<number> {
+  if (num < 2) return [];
+    let arr: Array<number> = [1,1];
+    let initialValue: number;
+      for (initialValue = arr[arr.length -1]; initialValue <= num; initialValue += arr[arr.length -2]) 
+        arr.push(initialValue)
     return arr
-  } else {
-    return `set parameter that will be a number that not eaqual 0 and more than 0 `
-  }
 }
-console.log(fib('Alabama'));
+
 console.log(fib(0));
 console.log(fib(12));
+
+
+
